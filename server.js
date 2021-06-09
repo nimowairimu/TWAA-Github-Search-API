f125900cf13c84f209c7536cafed7ba65aa2378c
nimowairimu <nimowairimu25@gmail.com>
2021-06-10 00:58:00 +0300

    Add path to redirect request to index.html.

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/githib-search'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/github-search/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+const app = express();
+app.use(requireHTTPS);
+app.use(express.static(/dist/github-search));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/github-search/'}),
+);
 app.listen(process.env.PORT || 8080);
